8668621d2db02410bc2ff8adb1d55ecee0ba567d
Thinus <thinus.barnard@payfast.co.za>
2022-11-02 17:42:12 +0200

    Changes

--- server.js
+++ server.js
@@ -2,24 +2,15 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-const config = require('./config.json')
 
-const ngrok = require('ngrok');
+// Listen on port 11111
+server.listen(11111, () => {
+    console.log(`server running at http://localhost:11111`);
+});
 
-ngrok.authtoken(config.ngrok_token);
-
-(async function() {
-    const url = await ngrok.connect(11111);
-
-    // Listen on port 11111
-    server.listen(11111, () => {
-        console.log(`server running at ${url}`);
+io.on('connection', socket => {
+    // Broadcast a user's message to everyone else in the room
+    socket.on('send', data => {
+        io.emit('message', data);
     });
-
-    io.on('connection', socket => {
-        // Broadcast a user's message to everyone else in the room
-        socket.on('send', data => {
-            io.emit('message', data);
-        });
-    });
-})();
+});
